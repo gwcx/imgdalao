ea67c855d7183a36cd11a319211d18825aa640cc
gwcx <gwcx@163.com>
2023-08-10 14:34:07 +0800

    更新

--- asset/js/upload.js
+++ asset/js/upload.js
@@ -224,10 +224,10 @@ function render_uploaded() {
         var wpurl = resp.data.thumb == null ? WPRAW + resp.data.url : WPRAW + resp.data.thumb.url;
 		var wpcfurl = resp.data.thumb == null ? WPCFRAW + resp.data.url : WPCFRAW + resp.data.thumb.url;
         var RAW = resp.data.thumb == null ? PhRAW + resp.data.url : PhRAW + resp.data.thumb.url;
-        $('#imagedetail').append(formatHtml({ url: url, code: url, wp: wpurl, wpcf: wpcfurl,raw: RAW }));
-        $('#htmlcode').append(formatHtml({ url: url, code: '<img src="' + url + '" />', wp: '<img src="' + wpurl + '" />',wpcf: '<img src="' + wpcfurl + '" />', raw: '<img src="' + RAW + '" />' }));
-        $('#bbcode').append(formatHtml({ url: url, code: '[img]' + url + '[/img]', wp: '[img]' + wpurl + '[/img]' ,wpcf: '[img]' + wpcfurl + '[/img]' , raw: '[img]' + RAW + '[/img]' }));
-        $('#markdown').append(formatHtml({ url: url, code: '![' + name + '](' + url + ')', wp: '![' + name + '](' + wpurl + ')',wpcf: '![' + name + '](' + wpcfurl + ')', raw: '![' + name + '](' + RAW + ')' }));
+        $('#imagedetail').append(formatHtml({ url: url, code: url, wp: wpurl, wpcf: wpcfurl, raw: RAW }));
+        $('#htmlcode').append(formatHtml({ url: url, code: '<img src="' + url + '" />', wp: '<img src="' + wpurl + '" />', wpcf: '<img src="' + wpcfurl + '" />', raw: '<img src="' + RAW + '" />' }));
+        $('#bbcode').append(formatHtml({ url: url, code: '[img]' + url + '[/img]', wp: '[img]' + wpurl + '[/img]' , wpcf: '[img]' + wpcfurl + '[/img]' , raw: '[img]' + RAW + '[/img]' }));
+        $('#markdown').append(formatHtml({ url: url, code: '![' + name + '](' + url + ')', wp: '![' + name + '](' + wpurl + ')', wpcf: '![' + name + '](' + wpcfurl + ')', raw: '![' + name + '](' + RAW + ')' }));
     });
 }
 
