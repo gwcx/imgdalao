72cfa3e94c05de76e234a748bbd94f5904d12ee4
gwcx <gwcx@163.com>
2023-08-12 21:10:10 +0800

    Update upload.js

--- asset/js/upload.js
+++ asset/js/upload.js
@@ -215,7 +215,7 @@ function render_uploaded() {
         var name = resp.data.url.split('/').pop();
         
         const WPRAW = "https://i3.wp.com/telegra.ph"  //使用wordpress.com加速
-		const WPCFRAW = "https://i3.wp.com/dalaoimg.pages.dev"  //使用wordpress.com加速cf
+		const WPCFRAW = "https://i3.wp.com/img-peach.vercel.app"  //使用wordpress.com加速cf,修改后方域名为你的加速域名
         const PhRAW = "https://telegra.ph"   //原地址
         const PROXYURL = ""  //自定义加速域名 默认是使用cloudflare
         const BASE_PROXYURL = PROXYURL ? PROXYURL : BASE_URL;
