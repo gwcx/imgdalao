7dad0200c910c2a524de1e41474e1b078d5b6e15
gwcx <gwcx@163.com>
2023-08-13 15:12:23 +0800

    更新亚洲api

--- asset/js/upload.js
+++ asset/js/upload.js
@@ -215,7 +215,7 @@ function render_uploaded() {
         var name = resp.data.url.split('/').pop();
         
         const WPRAW = "https://i3.wp.com/telegra.ph"  //使用wordpress.com加速
-		const WPCFRAW = "https://i3.wp.com/dalaoimg.pages.dev"  //使用wordpress.com加速cf,修改后方域名为你的加速域名
+		const WPCFRAW = "https://i3.wp.com/imgs.dalao.pro/api"  //使用wordpress.com加速cf,修改后方域名为你的加速域名
         const PhRAW = "https://telegra.ph"   //原地址
         const PROXYURL = ""  //自定义加速域名 默认是使用cloudflare
         const BASE_PROXYURL = PROXYURL ? PROXYURL : BASE_URL;
